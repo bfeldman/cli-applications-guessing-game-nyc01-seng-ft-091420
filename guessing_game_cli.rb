e9a831f3aa4d5818657b5f46d883b3a0340a39a1
Brian Feldman <brian.feldman42@gmail.com>
2020-08-29 01:52:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,6 @@
 # Code your solution here!
 def run_guessing_game
+<<<<<<< HEAD
   number = rand(6) + 1
   puts number
   puts "Enter a number between 1 and 6."
@@ -12,4 +13,7 @@ def run_guessing_game
   elsif input.to_i != number
     puts "Sorry! The computer guessed #{number}."
   end
+=======
+  number = rand(1..6)
+>>>>>>> d455df18da91eb18622ae70ae44c68db211421d4
 end
